bf0474d5fe05428e5ba24a3b93b35fce532009d6
biancapastos <biancapastos12@gmail.com>
2018-03-28 17:26:29 -0300

    pagina cabecalho

---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+<!-- Bianca -->
+<?php
+	
+	echo "<a href = 'index.php'>Home</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'calculadora.php'>Calculadora</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'imc.php'>Calcule seu IMC</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'form_cadastro_pessoa.php'>Cadastro</a>";
+	
+?>
